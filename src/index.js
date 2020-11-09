52a1e4a3c266ddb5cfaf389ecaa25fee8511115a
totalintelli <blueintelli@gmail.com>
2020-11-09 17:50:12 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 new Vue({
-  el: "#editor",
+  el: "#editor", // id가 editor인 태그에서 html tag를 만든다.
   data: {
-    input: "# 환영해요!!!",
+    input: "# 환영해요!!!", // data input 항목에 문자열을 입력한다.
   },
   computed: {
+    // compiledMarkdown v-html 항목에서 함수를 실행한다.
     compiledMarkdown: function () {
-      return marked(this.input, { sanitize: true });
+      return marked(this.input, { sanitize: true }); // marked.js의  marked 함수를 사용한다. 입력한 마크다운에 따라서 글자 디자인이 달라지게 한다.
     },
   },
   methods: {
     update: _.debounce(function (e) {
-      this.input = e.target.value;
-    }, 300),
+      // 입력한 시간동안 기다리는 debounced function을 생성한다.
+      this.input = e.target.value; // data input 항목을 입력한 값으로 수정한다.
+    }, 300), // 300 밀리세컨드를 기다린다.
   },
 });
